aa44c2ef424acfcdab8cd6d74bb4e18bd8d84413
Киричук Василь <w.kyrychuk@gmail.com>
2018-09-27 17:24:32 +0300

    temp

--- TeamProject/UnitTestProject1/Tests/Vasyl/temp.cs
+++ TeamProject/UnitTestProject1/Tests/Vasyl/temp.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace OpenCartTests.Tests.Vasyl
 {
-    class temp
+    [TestFixture]
+    public class UnitTest1
     {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
     }
 }
