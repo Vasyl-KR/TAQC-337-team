01ac26b0b405ed405ac384af29ee1a1274c421c3
Назар Лободинський <nazar.l1315@gmail.com>
2018-09-27 17:09:24 +0300

    3

---
+++ TeamProject/UnitTestProject1/Tests/Nazar/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCartTests.Tests.Nazar
+{
+    class Class1
+    {
+    }
+}
