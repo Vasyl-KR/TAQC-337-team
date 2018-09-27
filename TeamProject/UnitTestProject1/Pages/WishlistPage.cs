41981ea31d8ce7152c2d173615530adc9167c9a4
Pavlo <pristayko.pavlo@gmail.com>
2018-09-27 16:55:28 +0300

    add wishlist page

---
+++ TeamProject/UnitTestProject1/Pages/WishlistPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.Pages
+{
+    class Wishlist
+    {
+        //Wishlist by Pavlo
+    }
+}
