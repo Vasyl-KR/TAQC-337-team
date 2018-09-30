bb1c2aebb016753566b2d70a0846bb43b0c26445
Ostap <ostap024@gmail.com>
2018-09-30 19:38:02 +0300

    Add Account Page Edit loginpage

---
+++ TeamProject/OpenCart.Tests/Pages/AccountPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCartTests.Pages
+{
+    public class AccountPage : ATopComponent
+    {
+        public AccountPage(IWebDriver driver) : base(driver) { }
+    }
+}
