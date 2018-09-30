bfd5317d409295992ca4fba443f57443e5e566fb
Vasyl <w.kyrychuk@gmail.com>
2018-09-30 21:59:43 +0300

    add IWebDriverWait

--- TeamProject/OpenCart.Tests/Pages/LaptopsAndNotebooksPage.cs
+++ TeamProject/OpenCart.Tests/Pages/LaptopsAndNotebooksPage.cs
@@ -19,7 +19,7 @@ namespace OpenCartTests.Pages
 
         // Link locators
         private const string COMPARABLE_PRODUCT_LINK = "//div[@class = 'alert alert-success']/child::a"; //XPath
-        private const string PRODUCT_COMPARISON_LINK = "//a[contains (@href, 'compare')]"; //XPath
+        private const string PRODUCT_COMPARISON_LINK = "compare-total"; //XPath
         private const string FIRST_PRODUCT_LINK = ".caption h4:first-of-type"; //cssSelector
 
         // Buttons locators
@@ -33,7 +33,7 @@ namespace OpenCartTests.Pages
         public IWebElement ComparableProductLink
         { get { return driver.FindElement(By.XPath(COMPARABLE_PRODUCT_LINK)); } }
         public IWebElement ProductComparisonLink
-        { get { return driver.FindElement(By.XPath(PRODUCT_COMPARISON_LINK)); } }
+        { get { return driver.FindElement(By.Id(PRODUCT_COMPARISON_LINK)); } }
         public IWebElement FirstProductLink
         { get { return driver.FindElement(By.CssSelector(FIRST_PRODUCT_LINK)); } }
         public IWebElement CompareThisProductButton
@@ -76,6 +76,10 @@ namespace OpenCartTests.Pages
         {
             ComparableProductLink.Click();
         }
+        public string GetProductComparisonLinkText()
+        {
+            return ProductComparisonLink.Text;
+        }
 
         public void ClickProductComparisonLink()
         {
@@ -93,11 +97,13 @@ namespace OpenCartTests.Pages
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             Func<IWebDriver, bool> waitForElement = new Func<IWebDriver, bool>((IWebDriver drv) =>
             {
-                bool actual = SuccessAlertMessage.Enabled;
+                bool actual = GetProductComparisonLinkText().Contains("1");
                 return actual;
+
             });
-            //Thread.Sleep(2000);
             wait.Until(waitForElement);
+            //Thread.Sleep(2000);
+
             ClickProductComparisonLink();
             return new ProductComparisonPage(driver);
         }
