c28d1a310f70399608ad847dd9a9d9c570adaeba
Ostap <ostap024@gmail.com>
2018-09-30 19:54:24 +0300

    edit account page, add props

--- TeamProject/OpenCart.Tests/Pages/AccountPage.cs
+++ TeamProject/OpenCart.Tests/Pages/AccountPage.cs
@@ -9,6 +9,41 @@ namespace OpenCartTests.Pages
 {
     public class AccountPage : ATopComponent
     {
+        public IWebElement EditAccountInformation
+        { get { return driver.FindElement(By.XPath("//div[@id='content']//ul[@class='list-unstyled']//a[contains(@href,'account/edit')]")); } }
+
+        public IWebElement ChangePassword
+        { get { return driver.FindElement(By.XPath("//div[@id='content']//ul[@class='list-unstyled']//a[contains(@href,'account/password')]")); } }
+
+        public IWebElement ModifyAddressBook
+        { get { return driver.FindElement(By.XPath("//div[@id='content']//ul[@class='list-unstyled']//a[contains(@href,'account/address')]")); } }
+
+        public IWebElement ModifyWishList
+        { get { return driver.FindElement(By.XPath("//div[@id='content']//ul[@class='list-unstyled']//a[contains(@href,'account/wishlist')]")); } }
+
+        public IWebElement OrderHistory
+        { get { return driver.FindElement(By.XPath("//div[@id='content']//ul[@class='list-unstyled']//a[contains(@href,'account/order')]")); } }
+
+        public IWebElement Downloads
+        { get { return driver.FindElement(By.XPath("//div[@id='content']//ul[@class='list-unstyled']//a[contains(@href,'account/download')]")); } }
+
+        public IWebElement RewardPoints
+        { get { return driver.FindElement(By.XPath("//div[@id='content']//ul[@class='list-unstyled']//a[contains(@href,'account/reward')]")); } }
+
+        public IWebElement ReturnRequests
+        { get { return driver.FindElement(By.XPath("//div[@id='content']//ul[@class='list-unstyled']//a[contains(@href,'account/return')]")); } }
+
+        public IWebElement Transactions
+        { get { return driver.FindElement(By.XPath("//div[@id='content']//ul[@class='list-unstyled']//a[contains(@href,'account/transaction')]")); } }
+
+        public IWebElement RecurringPayments
+        { get { return driver.FindElement(By.XPath("//div[@id='content']//ul[@class='list-unstyled']//a[contains(@href,'account/recurring')]")); } }
+
+        public IWebElement SubUnsubToNewsletter
+        { get { return driver.FindElement(By.XPath("//div[@id='content']//ul[@class='list-unstyled']//a[contains(@href,'account/newsletter')]")); } }
+
+
+
         public AccountPage(IWebDriver driver) : base(driver) { }
     }
 }
