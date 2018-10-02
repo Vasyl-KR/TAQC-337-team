136cab0d6347c34adf9dfc99e4ec1c9aef2f0fee
Ostap <ostap024@gmail.com>
2018-10-02 13:56:49 +0300

    add base test

---
+++ TeamProject/OpenCart.Tests/Tests/BaseTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace OpenCartTests.Tests
+{
+
+    [TestFixture]
+    public abstract class BaseTest
+    {
+        protected IWebDriver driver;
+        protected IWebDriver Driver
+        {
+            get
+            {
+                if (driver == null)
+                {
+                    driver = new ChromeDriver();
+                }
+                return driver;
+            }
+            set
+            {
+                driver = (dynamic)value;
+            }
+        }
+
+        [TearDown]
+        public void TearDown() { }
+
+        [SetUp]
+        public void SetUp() { }
+    }
+}
