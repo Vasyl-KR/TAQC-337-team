00301169b71ef0023a0157be56fab10e066fe336
vtereshtc <wtereshko@gmail.com>
2018-10-02 13:23:09 +0300

    Add Pages class

---
+++ TeamProject/OpenCart.Tests/Pages/Pages.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCartTests.Pages
+{
+   public class Pages
+    {
+        private readonly IWebDriver driver;
+
+        public Pages (IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+   
+    }
+}
