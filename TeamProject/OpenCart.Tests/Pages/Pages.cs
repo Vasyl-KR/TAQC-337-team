9aefcdd4f2e1f7be9a84914e2492df9597b950c0
Volodymyr Tereshko <wtereshko@gmail.com>
2018-10-03 21:52:01 +0300

    Modification Pages class

--- TeamProject/OpenCart.Tests/Pages/Pages.cs
+++ TeamProject/OpenCart.Tests/Pages/Pages.cs
@@ -1,10 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OpenCartTests.Pages
 {
@@ -12,107 +8,44 @@ namespace OpenCartTests.Pages
     {
         #region Fields
 
-        private readonly IWebDriver driver;
-        private AccountPage accountPage;
-        private AddressPage addressPage;
-        private ChangePasswordPage changePasswordPage;
-        private EditAccountInformationPage editAccountInformationPage;
-        private EditAddressPage editAddressPage;
-        private HomePage homePage;
-        private LaptopsAndNotebooksPage laptopsAndNotebooksPage;
-        private LoginPage loginPage;
-        private LogoutPage logoutPage;
-        private ProductComparisonPage productComparisonPage;
-        private RegisterPage registerPage;
-        private RepeatLoginPage repeatLoginPage;
-        private SearchPage searchPage;
-        private WishlistPage wishlistPage;
-        private RepeatAccountPage repeatAccountPage;
-
-
         private WebDriverWait wait;
+        private IWebDriver driver;
 
         #endregion
 
         #region Proporties
 
-        public AccountPage AccountPage
-        {
-            get { return accountPage ?? new AccountPage(driver); }
-        }
-        
-        public AddressPage AddressPage
-        {
-            get { return addressPage ?? new AddressPage(driver); }
-        }
+        public AccountPage AccountPage => new AccountPage(driver);
 
-        public ChangePasswordPage ChangePasswordPage
-        {
-            get { return changePasswordPage ?? new ChangePasswordPage(driver); }
-        }
+        public AddressPage AddressPage => new AddressPage(driver);
 
-        public ConfirmationRegisterPage ConfirmationRegisterPage
-        {
-            get { return new ConfirmationRegisterPage(driver); }
-        }
+        public ChangePasswordPage ChangePasswordPage => new ChangePasswordPage(driver);
 
-        public EditAccountInformationPage EditAccountInformationPage
-        {
-            get { return editAccountInformationPage ?? new EditAccountInformationPage(driver); }
-        }
+        public ConfirmationRegisterPage ConfirmationRegisterPage => new ConfirmationRegisterPage(driver);
 
-        public EditAddressPage EditAddressPage
-        {
-            get { return editAddressPage ?? new EditAddressPage(driver); }
-        }
-        public HomePage HomePage
-        {
-            get { return homePage ?? new HomePage(driver); }
-        }
+        public EditAccountInformationPage EditAccountInformationPage => new EditAccountInformationPage(driver);
 
-        public LaptopsAndNotebooksPage LaptopsAndNotebooksPage
-        {
-            get { return laptopsAndNotebooksPage ?? new LaptopsAndNotebooksPage(driver); }
-        }
+        public EditAddressPage EditAddressPage => new EditAddressPage(driver);
 
-        public LoginPage LoginPage
-        {
-            get { return loginPage ?? new LoginPage(driver); }
-        }
+        public HomePage HomePage => new HomePage(driver);
 
-        public LogoutPage LogoutPage
-        {
-            get { return logoutPage ?? new LogoutPage(driver); }
-        }
+        public LaptopsAndNotebooksPage LaptopsAndNotebooksPage => new LaptopsAndNotebooksPage(driver);
 
-        public ProductComparisonPage ProductComparisonPage
-        {
-            get { return productComparisonPage ?? new ProductComparisonPage(driver); }
-        }
+        public LoginPage LoginPage => new LoginPage(driver);
 
-        public RegisterPage RegisterPage
-        {
-            get { return registerPage ?? new RegisterPage(driver); }
-        }
+        public LogoutPage LogoutPage => new LogoutPage(driver);
 
-        public RepeatLoginPage RepeatLoginPage
-        {
-            get { return repeatLoginPage ?? new RepeatLoginPage(driver); }
-        }
-        public RepeatAccountPage RepeatAccountPage
-        {
-            get { return repeatAccountPage ?? new RepeatAccountPage(driver); }
-        }
+        public ProductComparisonPage ProductComparisonPage => new ProductComparisonPage(driver);
 
-        public SearchPage SearchPage
-        {
-            get { return searchPage ?? new SearchPage(driver); }
-        }
+        public RegisterPage RegisterPage => new RegisterPage(driver);
 
-        public WishlistPage WishlistPage
-        {
-            get { return wishlistPage ?? new WishlistPage(driver); }
-        }
+        public RepeatLoginPage RepeatLoginPage => new RepeatLoginPage(driver);
+
+        public RepeatAccountPage RepeatAccountPage => new RepeatAccountPage(driver);
+
+        public SearchPage SearchPage => new SearchPage(driver);
+
+        public WishlistPage WishlistPage => new WishlistPage(driver);
 
         #endregion
 
@@ -121,29 +54,9 @@ namespace OpenCartTests.Pages
         public Pages (IWebDriver driver)
         {
             this.driver = driver;
-            // PageInitializator();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
         }
         #endregion
-        //add all new pages
-        private void PageInitializator()
-        {
-            accountPage = new AccountPage(driver);
-            addressPage = new AddressPage(driver);
-            changePasswordPage = new ChangePasswordPage(driver);
-            editAccountInformationPage = new EditAccountInformationPage(driver);
-            editAddressPage = new EditAddressPage(driver);
-            homePage = new HomePage(driver);
-            laptopsAndNotebooksPage = new LaptopsAndNotebooksPage(driver);
-            loginPage = new LoginPage(driver);
-            logoutPage = new LogoutPage(driver);
-            productComparisonPage = new ProductComparisonPage(driver);
-            registerPage = new RegisterPage(driver);
-            repeatLoginPage = new RepeatLoginPage(driver);
-            repeatAccountPage = new RepeatAccountPage(driver);
-            searchPage = new SearchPage(driver);
-            wishlistPage = new WishlistPage(driver);
-        }
 
         public IWebElement WaitForElementTextContains(IWebElement webElement,string expectedStr)
         {
