b416d4405525d78a00a3557a0c7c9ac959a454c3
Volodymyr Tereshko <wtereshko@gmail.com>
2018-10-05 10:26:43 +0300

    PageList changes Proporties from lambda to dody style

--- TeamProject/OpenCart.Tests/Pages/PagesList.cs
+++ TeamProject/OpenCart.Tests/Pages/PagesList.cs
@@ -1,6 +1,4 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
-using System;
 
 namespace OpenCartTests.Pages
 {
@@ -15,33 +13,75 @@ namespace OpenCartTests.Pages
 
         #region Proporties
 
-        public AccountPage AccountPage => new AccountPage(driver);
+        public AccountPage AccountPage
+        {
+            get { return new AccountPage(driver); }
+        }
 
-        public AddressPage AddressPage => new AddressPage(driver);
+        public AddressPage AddressPage
+        {
+            get { return new AddressPage(driver); }
+        }
 
-        public ChangePasswordPage ChangePasswordPage => new ChangePasswordPage(driver);
+        public ChangePasswordPage ChangePasswordPage
+        {
+            get { return new ChangePasswordPage(driver); }
+        }
 
-        public ConfirmationRegisterPage ConfirmationRegisterPage => new ConfirmationRegisterPage(driver);
+        public ConfirmationRegisterPage ConfirmationRegisterPage
+        {
+            get { return new ConfirmationRegisterPage(driver); }
+        }
 
-        public EditAccountInformationPage EditAccountInformationPage => new EditAccountInformationPage(driver);
+        public EditAccountInformationPage EditAccountInformationPage
+        {
+            get { return new EditAccountInformationPage(driver); }
+        }
 
-        public EditAddressPage EditAddressPage => new EditAddressPage(driver);
+        public EditAddressPage EditAddressPage
+        {
+            get { return new EditAddressPage(driver); }
+        }
 
-        public HomePage HomePage => new HomePage(driver);
+        public HomePage HomePage
+        {
+            get { return new HomePage(driver); }
+        }
 
-        public LaptopsAndNotebooksPage LaptopsAndNotebooksPage => new LaptopsAndNotebooksPage(driver);
+        public LaptopsAndNotebooksPage LaptopsAndNotebooksPage
+        {
+            get { return new LaptopsAndNotebooksPage(driver); }
+        }
 
-        public LoginPage LoginPage => new LoginPage(driver);
+        public LoginPage LoginPage
+        {
+            get { return new LoginPage(driver); }
+        }
 
-        public LogoutPage LogoutPage => new LogoutPage(driver);
+        public LogoutPage LogoutPage
+        {
+            get { return new LogoutPage(driver); }
+        }
 
-        public ProductComparisonPage ProductComparisonPage => new ProductComparisonPage(driver);
+        public ProductComparisonPage ProductComparisonPage
+        {
+            get { return new ProductComparisonPage(driver); }
+        }
 
-        public RegisterPage RegisterPage => new RegisterPage(driver);
+        public RegisterPage RegisterPage
+        {
+            get { return new RegisterPage(driver); }
+        }
 
-        public SearchPage SearchPage => new SearchPage(driver);
+        public SearchPage SearchPage
+        {
+            get { return new SearchPage(driver); }
+        }
 
-        public WishlistPage WishlistPage => new WishlistPage(driver);
+        public WishlistPage WishlistPage
+        {
+            get { return new WishlistPage(driver); }
+        }
 
         #endregion
 
