31eb6f8122e1bc589388490948506adecac97dde
ostap024 <40698896+ostap024@users.noreply.github.com>
2018-10-08 11:29:38 +0300

    Update Passwords.cs

--- TeamProject/OpenCart.Tests/Tests/Ostap/ChangePasswordTests/Passwords.cs
+++ TeamProject/OpenCart.Tests/Tests/Ostap/ChangePasswordTests/Passwords.cs
@@ -8,17 +8,5 @@
         public const string TooShort = "Qwe";
         public const string TooLong = "Qwerty12345678901234567890/";
         public const string Empty = "";
-        //public static readonly Passwords Correct1 = new Passwords("Qwerty1234/");
-        //public static readonly Passwords Correct2 = new Passwords("Q1w2e3r4t5y6/");
-        //public static readonly Passwords Correct3 = new Passwords("Asdfgh1234/");
-        //public static readonly Passwords TooShort = new Passwords("Qwe");
-        //public static readonly Passwords TooLong = new Passwords("Qwerty12345678901234567890/");
-        //private Passwords(string value)
-        //{
-        //    Value = value;
-        //}
-        //public string Value { get; private set; }
-
-        //public static implicit operator string(Passwords p) { return p.Value; }
     }
 }
